cabaace11f74e4bcb45e7dbbaff88e47ce0808c0
selvakannan <4mselva@gmail.com>
2024-02-23 16:50:31 +0530

    remove comments and change controller name

--- src/main/kotlin/com/officelunchmanagement/MemberController.kt
+++ src/main/kotlin/com/officelunchmanagement/MemberController.kt
@@ -7,23 +7,9 @@ import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.*
 import java.sql.Date
 
-//@Controller("/member")
-//class HelloController {
-//
-//    @Get
-//    @Produces(MediaType.TEXT_PLAIN)
-//    fun index() = "Hello World"
-//}
 
-//enum class status{
-//    YES, NO
-//}
-
-
-//data class MyData(val name: String)
-//
 @Controller("/member")
-class MyController {
+class MemberController {
 
     @Post
     fun processData(id: Int, name: String, choice: String): HttpResponse<String>{
