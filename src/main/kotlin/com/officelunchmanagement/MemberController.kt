f98326f30733f28108982045bde48ed159a77008
Vishwanath N <vishwanathnarayanan29@gmail.com>
2024-02-24 18:10:39 +0530

    Add add preference endpoint to handle post request

--- src/main/kotlin/com/officelunchmanagement/MemberController.kt
+++ src/main/kotlin/com/officelunchmanagement/MemberController.kt
@@ -6,10 +6,12 @@ import classes.EmployeeAttendance
 import classes.RecordResponse
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.MediaType
+import io.micronaut.http.MutableHttpResponse
 import io.micronaut.http.annotation.*
 import jakarta.inject.Inject
 import netscape.javascript.JSObject
 import java.sql.Date
+import kotlin.math.log
 
 
 @Controller("/member")
@@ -17,6 +19,11 @@ class MemberController {
 
     @Inject
     lateinit var memberAttendance: EmployeeAttendance
+    @Post("/addpreference")
+    fun addPreference(@Body memberPreference:RecordResponse): HttpResponse<String>? {
+        val record = RecordResponse(memberPreference.id,memberPreference.name,memberPreference.status)
+        return HttpResponse.ok("Preference Saved Successfully");
+    }
     @Post("/check")
     fun CheckAndReturnRecord(id:String,name: String): RecordResponse {
         val (memberId,memberName,status)= memberAttendance.returnRecordIfExistsOrSendDefault(id,name)
