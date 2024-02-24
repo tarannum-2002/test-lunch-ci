e4ab50a797a754c70a6ae132f4f2ca6953020103
Vishwanath N <vishwanathnarayanan29@gmail.com>
2024-02-24 18:59:32 +0530

    Add add preference

--- src/main/kotlin/com/officelunchmanagement/MemberController.kt
+++ src/main/kotlin/com/officelunchmanagement/MemberController.kt
@@ -3,32 +3,28 @@ package com.officelunchmanagement
 
 
 import classes.EmployeeAttendance
-import classes.RecordResponse
+import classes.MemberPreference
 import io.micronaut.http.HttpResponse
-import io.micronaut.http.MediaType
-import io.micronaut.http.MutableHttpResponse
 import io.micronaut.http.annotation.*
 import jakarta.inject.Inject
-import netscape.javascript.JSObject
-import java.sql.Date
-import kotlin.math.log
 
 
 @Controller("/member")
 class MemberController {
 
     @Inject
-    lateinit var memberAttendance: EmployeeAttendance
+    var memberAttendance= EmployeeAttendance()
     @Post("/addpreference")
-    fun addPreference(@Body memberPreference:RecordResponse): HttpResponse<String>? {
-        val record = RecordResponse(memberPreference.id,memberPreference.name,memberPreference.status)
+    fun addPreference(@Body memberPreference:MemberPreference): HttpResponse<String>? {
+        val record = MemberPreference(memberPreference.id,memberPreference.name,memberPreference.status)
+        memberAttendance.insertNewRecord(record)
         return HttpResponse.ok("Preference Saved Successfully");
     }
+
     @Post("/check")
-    fun CheckAndReturnRecord(id:String,name: String): RecordResponse {
-        val (memberId,memberName,status)= memberAttendance.returnRecordIfExistsOrSendDefault(id,name)
-        val response = RecordResponse(memberId,memberName,status)
-        return response
+    fun CheckAndReturnRecord(id: String,name:String): MemberPreference? {
+        val record = memberAttendance.returnRecordIfExistsOrSendDefault(id,name)
+        return record
     }
 
 }
