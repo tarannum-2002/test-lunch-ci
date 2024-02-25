c662f78dc8fdb8670092c00de57c0c326943e5c3
tarannum-2002 <tarannums@sahaj.ai>
2024-02-25 13:02:56 +0530

    modify count controller to get only members with yes

--- src/main/kotlin/com/officelunchmanagement/AdminController.kt
+++ src/main/kotlin/com/officelunchmanagement/AdminController.kt
@@ -1,15 +1,12 @@
 package com.officelunchmanagement
 
-import classes.EmployeeAttendance
-import classes.MemberPreference
 //import classes.RecordResponse
+import classes.EmployeeAttendance
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.MutableHttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
 import jakarta.inject.Inject
-
-
 
 
 @Controller("/admin")
@@ -19,8 +16,16 @@ class AdminController{
     var memberAttendance = EmployeeAttendance()
 
     @Post("/count")
-    fun getCount(): MutableHttpResponse<Map<String, MutableMap<String, MemberPreference>>>? {
-        val count = memberAttendance.getEmployeeRecords()
+    fun getCount(): MutableHttpResponse<Map<String, Int>>? {
+        var count = 0
+
+        val members = memberAttendance.getEmployeeRecords()
+        for (memberPreference in members.values) {
+            if (memberPreference.status == "Yes") {
+                count++
+            }
+        }
+
         val response = mapOf("count" to count)
         return HttpResponse.ok(response)
     }
